d54f3b9f763b8915dc8ef5fe284001e230f933cc
Yaron Zinar <yziner@preempt.com>
2020-09-17 18:52:31 +0300

    Adding scanner for CVE-2020-1472

--- scan.py
+++ scan.py
@@ -47,12 +47,104 @@ import codecs
 import calendar
 import struct
 import time
+import datetime
+import random
 from impacket import version
+from impacket.krb5.asn1 import seq_set, seq_set_iter, EncryptedData, KERB_PA_PAC_REQUEST, PA_ENC_TS_ENC, AS_REQ
+from impacket.krb5.kerberosv5 import sendReceive
 from impacket.examples.logger import ImpacketFormatter
 from impacket.smbconnection import SMBConnection, SessionError
+from impacket.krb5.types import Principal, KerberosTime
+from pyasn1.codec.der import encoder, decoder
+from impacket.krb5 import constants
 from impacket.smb3structs import *
+from impacket.krb5.crypto import Key, _enctype_table
 from impacket import ntlm
 from impacket.ntlm import AV_PAIRS, NTLMSSP_AV_EOL, NTLMSSP_AV_TIME, NTLMSSP_AV_FLAGS, NTOWFv2, NTLMSSP_AV_TARGET_NAME, NTLMSSP_AV_HOSTNAME,USE_NTLMv2, hmac_md5
+from pyasn1.type.univ import noValue
+
+def verify_kerberos_password(user, password, domain, kdc_host=None, request_pac=True, host_names=None, source_ip=None):
+    host_names = host_names or []
+    clientName = Principal(user, type=constants.PrincipalNameType.NT_PRINCIPAL.value)
+
+    domain = domain.upper()
+    serverName = Principal("krbtgt/%s" % domain, type=constants.PrincipalNameType.NT_PRINCIPAL.value)
+
+    pacRequest = KERB_PA_PAC_REQUEST()
+    pacRequest["include-pac"] = request_pac
+    encodedPacRequest = encoder.encode(pacRequest)
+
+    enctype = constants.EncryptionTypes.rc4_hmac.value
+    encryptionTypesData = None # RC4 doesn"t have salt
+    cipher = _enctype_table[enctype]
+
+    salt = encryptionTypesData[enctype] if encryptionTypesData else ''
+    key = cipher.string_to_key(password, salt, None)
+
+    # Let"s build the timestamp
+    timeStamp = PA_ENC_TS_ENC()
+
+    now = datetime.datetime.utcnow()
+    timeStamp["patimestamp"] = KerberosTime.to_asn1(now)
+    timeStamp["pausec"] = now.microsecond
+
+    encodedTimeStamp = encoder.encode(timeStamp)
+
+    # Key Usage 1
+    # AS-REQ PA-ENC-TIMESTAMP padata timestamp, encrypted with the
+    # client key (Section 5.2.7.2)
+    encriptedTimeStamp = cipher.encrypt(key, 1, encodedTimeStamp, None)
+
+    encryptedData = EncryptedData()
+    encryptedData["etype"] = cipher.enctype
+    encryptedData["cipher"] = encriptedTimeStamp
+    encodedEncryptedData = encoder.encode(encryptedData)
+
+    # Now prepare the new AS_REQ again with the PADATA
+    # ToDo: cannot we reuse the previous one?
+    asReq = AS_REQ()
+
+    asReq['pvno'] = 5
+    asReq['msg-type'] = int(constants.ApplicationTagNumbers.AS_REQ.value)
+
+    asReq['padata'] = noValue
+    asReq['padata'][0] = noValue
+    asReq['padata'][0]['padata-type'] = int(constants.PreAuthenticationDataTypes.PA_ENC_TIMESTAMP.value)
+    asReq['padata'][0]['padata-value'] = encodedEncryptedData
+
+    asReq['padata'][1] = noValue
+    asReq['padata'][1]['padata-type'] = int(constants.PreAuthenticationDataTypes.PA_PAC_REQUEST.value)
+    asReq['padata'][1]['padata-value'] = encodedPacRequest
+
+    reqBody = seq_set(asReq, 'req-body')
+
+    opts = list()
+    opts.append(constants.KDCOptions.forwardable.value)
+    opts.append(constants.KDCOptions.renewable.value)
+    opts.append(constants.KDCOptions.proxiable.value)
+    reqBody["kdc-options"] = constants.encodeFlags(opts)
+
+    seq_set(reqBody, "sname", serverName.components_to_asn1)
+    seq_set(reqBody, "cname", clientName.components_to_asn1)
+
+    reqBody["realm"] = domain
+
+    now = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+    reqBody["till"] = KerberosTime.to_asn1(now)
+    reqBody["rtime"] = KerberosTime.to_asn1(now)
+    reqBody["nonce"] = random.getrandbits(31)
+
+    seq_set_iter(reqBody, "etype", ((int(cipher.enctype),)))
+
+    try:
+        tgt = sendReceive(encoder.encode(asReq), domain, kdc_host)
+    except Exception, e:
+        if str(e).find('KDC_ERR_PREAUTH_FAILED') >= 0:
+            return False
+        raise
+
+    return True
+
 
 class mod_AV_PAIRS:
     def __init__(self, data = None):
@@ -198,37 +290,49 @@ class checker(object):
         except:
             logging.error('Error during connection to {}. TCP/445 refused, timeout?'.format(remote_host))
 
-    def check(self, remote_host):
+    def check(self, remote_names):
         # Validate credentials first
         if not self.creds_validated:
-            self.validate_creds(remote_host)
+            self.validate_creds(remote_names[0])
             self.creds_validated = True
 
-        # Now start scanner
-        try:
-            smbClient = SMBConnection(remote_host, remote_host, sess_port=int(self.__port)) #, preferredDialect=SMB2_DIALECT_21
-        except:
-            return
-        try:
-            # Both cve-2019-1019 and cve-2019-1040 were part of the same KB and can be checked
-            # by using cve-2019-1040 can logic
-            if self.__vuln in ["CVE-2019-1019", "CVE-2019-1040"]:
-                ntlm.computeResponseNTLMv2 = mod_cve20191040_computeResponseNTLMv2
-            if self.__vuln == "CVE-2019-1166":
-                ntlm.computeResponseNTLMv2 = mod_cve20191166_computeResponseNTLMv2
-            if self.__vuln == "CVE-2019-1338":
-                ntlm.computeResponseNTLMv2 = mod_cve20191338_computeResponseNTLMv2
-            smbClient.login(self.__username, self.__password, self.__domain, self.__lmhash, self.__nthash)
-            logging.info('Target %s is VULNERABLE to %s (authentication was accepted)', remote_host, self.__vuln)
-        except SessionError as exc:
-            if 'STATUS_INVALID_PARAMETER' in str(exc) and self.__vuln in ["CVE-2019-1019", "CVE-2019-1040", "CVE-2019-1166"]:
-                logging.info('Target %s is not vulnerable to %s (authentication was rejected)', remote_host, self.__vuln)
-            elif 'STATUS_LOGON_FAILURE' in str(exc) and self.__vuln == "CVE-2019-1338":
-                logging.info('Target %s is not vulnerable to %s (authentication was rejected)', remote_host, self.__vuln)
-            else:
-                logging.warning('Unexpected Exception while authenticating to %s: %s', remote_host, exc)
+        # CVE-2020-1472 scan is continuous
+        if self.__vuln == "CVE-2020-1472":
+            while True:
+                for remote_host in remote_names:
+                    if verify_kerberos_password(remote_host.split(".")[0]+"$", "", ".".join(remote_host.split(".")[1:]), remote_host):
+                        logging.info('Target %s was exploited for CVE-2020-1472!', remote_host)
+                    else:
+                        logging.info('Target %s was not exploited', remote_host)
+                    time.sleep(1)
 
-        smbClient.close()
+
+        # Now start scanner
+        for remote_host in remote_names:
+            try:
+                smbClient = SMBConnection(remote_host, remote_host, sess_port=int(self.__port)) #, preferredDialect=SMB2_DIALECT_21
+            except:
+                return
+            try:
+                # Both cve-2019-1019 and cve-2019-1040 were part of the same KB and can be checked
+                # by using cve-2019-1040 can logic
+                if self.__vuln in ["CVE-2019-1019", "CVE-2019-1040"]:
+                    ntlm.computeResponseNTLMv2 = mod_cve20191040_computeResponseNTLMv2
+                if self.__vuln == "CVE-2019-1166":
+                    ntlm.computeResponseNTLMv2 = mod_cve20191166_computeResponseNTLMv2
+                if self.__vuln == "CVE-2019-1338":
+                    ntlm.computeResponseNTLMv2 = mod_cve20191338_computeResponseNTLMv2
+                smbClient.login(self.__username, self.__password, self.__domain, self.__lmhash, self.__nthash)
+                logging.info('Target %s is VULNERABLE to %s (authentication was accepted)', remote_host, self.__vuln)
+            except SessionError as exc:
+                if 'STATUS_INVALID_PARAMETER' in str(exc) and self.__vuln in ["CVE-2019-1019", "CVE-2019-1040", "CVE-2019-1166"]:
+                    logging.info('Target %s is not vulnerable to %s (authentication was rejected)', remote_host, self.__vuln)
+                elif 'STATUS_LOGON_FAILURE' in str(exc) and self.__vuln == "CVE-2019-1338":
+                    logging.info('Target %s is not vulnerable to %s (authentication was rejected)', remote_host, self.__vuln)
+                else:
+                    logging.warning('Unexpected Exception while authenticating to %s: %s', remote_host, exc)
+
+            smbClient.close()
 
 # Process command-line arguments.
 def main():
@@ -248,7 +352,7 @@ def main():
     parser = argparse.ArgumentParser(description="NTLM scanner - Connects over SMB and attempts to authenticate "
                                                  "with invalid NTLM packets. If accepted, target is vulnerable to the scanned vulnerability")
 
-    parser.add_argument('target', action='store', help='[[domain/]username[:password]@]<targetName or address>')
+    parser.add_argument('-target', action='store', help='[[domain/]username[:password]@]<targetName or address>')
 
     group = parser.add_argument_group('connection')
 
@@ -259,9 +363,9 @@ def main():
                             ' the command line (you must still specify something as target name)')
     group.add_argument('-port', choices=['139', '445'], nargs='?', default='445', metavar="destination port",
                        help='Destination port to connect to SMB Server')
-    group.add_argument('-vuln', choices=['CVE-2019-1019', 'CVE-2019-1040', 'CVE-2019-1166', 'CVE-2019-1338'],
+    group.add_argument('-vuln', choices=['CVE-2019-1019', 'CVE-2019-1040', 'CVE-2019-1166', 'CVE-2019-1338', 'CVE-2020-1472'],
                        nargs='?', default='CVE-2019-1019', metavar="scanned vulnerability",
-                       help='The vulnerability to scan SMB Server on [CVE-2019-1019|CVE-2019-1040|CVE-2019-1166|CVE-2019-1338]')
+                       help='The vulnerability to scan SMB Server on [CVE-2019-1019|CVE-2019-1040|CVE-2019-1166|CVE-2019-1338|CVE-2020-1472]')
 
     group = parser.add_argument_group('authentication')
 
@@ -275,8 +379,19 @@ def main():
 
     import re
 
-    domain, username, password, remote_name = re.compile('(?:(?:([^/@:]*)/)?([^@:]*)(?::([^@]*))?@)?(.*)').match(
-        options.target).groups('')
+    domain = ""
+    username = ""
+    password = ""
+    remote_name = ""
+    if options.target is not None:
+        domain, username, password, remote_name = re.compile('(?:(?:([^/@:]*)/)?([^@:]*)(?::([^@]*))?@)?(.*)').match(options.target).groups('')
+    else:
+        if options.target_file is None:
+            logging.error("Please supply a target")
+            return
+        if options.vuln != "CVE-2020-1472":
+            logging.error("Please supply a username/password (you can't use this scanner with anonymous authentication)")
+            return
 
     #In case the password contains '@'
     if '@' in remote_name:
@@ -286,15 +401,19 @@ def main():
     if domain is None:
         domain = ''
 
-    if password == '' and username == '':
-        logging.error("Please supply a username/password (you can't use this scanner with anonymous authentication)")
-        return
-
-    if password == '' and username != '' and options.hashes is None:
-        from getpass import getpass
-        password = getpass("Password:")
-
     remote_names = []
+    creds_validated = False
+    if options.vuln != "CVE-2020-1472":
+        if password == '' and username == '':
+            logging.error("Please supply a username/password (you can't use this scanner with anonymous authentication)")
+            return
+
+        if password == '' and username != '' and options.hashes is None:
+            from getpass import getpass
+            password = getpass("Password:")
+    else:
+        creds_validated = True
+
     if options.target_file is not None:
         with open(options.target_file, 'r') as inf:
             for line in inf:
@@ -303,11 +422,8 @@ def main():
         remote_names.append(remote_name)
 
     lookup = checker(username, password, domain, int(options.port), options.hashes, options.vuln)
-    for remote_name in remote_names:
-        try:
-            lookup.check(remote_name)
-        except KeyboardInterrupt:
-            break
+    lookup.creds_validated = creds_validated
+    lookup.check(remote_names)
 
 
 if __name__ == '__main__':
